7bcb1eafc60019f6df8055671eb609222b908f66
GustavoMulin <gustavobarbosa7824@gmail.com>
2025-06-03 11:30:28 -0400

    [add]: Criando as rotas api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,13 @@
 <?php
 
+use App\Http\Controllers\ConsultaController;
+use App\Http\Controllers\MedicoController;
+use App\Http\Controllers\PacienteController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::prefix('vi')->group(function () {
+    Route::apiResource('pacientes', PacienteController::class);
+    Route::apiResource('medicos', MedicoController::class);
+    Route::apiResource('consultas', ConsultaController::class);
+});
